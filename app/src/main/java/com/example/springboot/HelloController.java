8ad16ca0574c2aa9428efe5859454deaef5f2980
bechirsouli <150070873+bechirsouli@users.noreply.github.com>
2023-11-16 02:52:59 +0100

    a

--- app/src/main/java/com/example/springboot/HelloController.java
+++ app/src/main/java/com/example/springboot/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
